1c5036c3d93c0f67edc8ce75b31b660cde1bd880
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2025-11-13 07:53:37 -0600

    New BBT Predicted Fanon portrait

--- Baldi's Basics Plus Modded Playable Characters/Patches/BCPPRelatedPatches.cs
+++ Baldi's Basics Plus Modded Playable Characters/Patches/BCPPRelatedPatches.cs
@@ -47,18 +47,20 @@ class DwellerAlreadyUnlockedCheck
 [ConditionalPatchMod("alexbw145.baldiplus.bcarnellchars"), HarmonyPatch(typeof(Pickup))]
 class DwellerAbility
 {
-    internal static Dictionary<ItemObject, Sprite> crisped = new Dictionary<ItemObject, Sprite>();
+    private static readonly Dictionary<ItemObject, Sprite> crisped = new Dictionary<ItemObject, Sprite>();
     [HarmonyPatch("Start")]
     [HarmonyPatch(nameof(Pickup.AssignItem))]
-    static void Postfix(Pickup __instance)
+    private static void Postfix(Pickup __instance)
     {
         if (PlayableCharsPlugin.IsRandom && !CoreGameManager.Instance.readyToStart) return;
         if (CoreGameManager.Instance.GetPlayer(0) != null && PlrPlayableCharacterVars.GetLocalPlayable()?.GetCurrentPlayable().name.ToLower().Replace(" ", "") == "thedweller"
             && __instance.icon != null && __instance.item.itemType != Items.None) {
             Sprite crisp = crisped.ContainsKey(__instance.item) ? crisped[__instance.item] : Sprite.Create(__instance.item.itemSpriteLarge.texture, new Rect(0f, 0f, __instance.item.itemSpriteLarge.texture.width, __instance.item.itemSpriteLarge.texture.height), Vector2.one / 2f, 80f, 0u, SpriteMeshType.FullRect);
-            crisp.name = "CrispedItemIconSpr_" + __instance.item.name;
             if (!crisped.ContainsKey(__instance.item))
+            {
+                crisp.name = "CrispedItemIconSpr_" + __instance.item.name;
                 crisped.Add(__instance.item, crisp);
+            }
             __instance.icon.spriteRenderer.sprite = crisp;
             //__instance.icon.spriteRenderer.material = PlayableCharsPlugin.assetMan.Get<Material>("DwellerMapMat");
         }
